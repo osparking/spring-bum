3262c179df9b94f7b81bf6016afe5f09c6336d75
osparking <jbpark03@gmail.com>
2024-01-22 19:45:47 +0900

    승객 정보 속성인 국가 영역 클래스

---
+++ src/main/java/space/jbpark/jdbc_bum/model/Country.java
@@ -0,0 +1,17 @@
+package space.jbpark.jdbc_bum.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Country {
+  private String name;
+  private String codeName;
+}
