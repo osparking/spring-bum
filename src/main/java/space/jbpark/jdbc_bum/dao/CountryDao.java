a0e0473fcb85420e5fd31cbc92ccc53c47e3c7d6
osparking <jbpark03@gmail.com>
2024-01-23 21:58:43 +0900

    국가 테이블 Dao - 모든 국가 목록 채취

---
+++ src/main/java/space/jbpark/jdbc_bum/dao/CountryDao.java
@@ -0,0 +1,38 @@
+package space.jbpark.jdbc_bum.dao;
+
+import static space.jbpark.jdbc_bum.ConnectionManager.closeConnection;
+import static space.jbpark.jdbc_bum.ConnectionManager.openConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import space.jbpark.jdbc_bum.model.Country;
+
+public class CountryDao {
+	private static final String GET_ALL_COUNTRIES = "select * from country";
+
+	public List<Country> getCountries() {
+		List<Country> countries = new ArrayList<>();
+
+		try {
+			Connection connection = openConnection();
+			PreparedStatement statement = connection
+					.prepareStatement(GET_ALL_COUNTRIES);
+			ResultSet result = statement.executeQuery();
+			while (result.next()) {
+				countries.add(new Country(result.getString("name"),
+						result.getString("codeName")));
+			}
+			statement.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			closeConnection();
+		}
+		return countries;
+	}
+}
