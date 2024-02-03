265ed2e2f0daae0f997928d4b87ef26f1231d575
osparking <jbpark03@gmail.com>
2024-02-03 09:44:07 +0900

    봄철에서 JDBC 지원을 사용한 DAO 메소드 간소화

--- src/test/java/space/jbpark/jdbc_bum/CountriesLoader.java
+++ src/test/java/space/jbpark/jdbc_bum/CountriesLoader.java
@@ -1,36 +1,21 @@
 package space.jbpark.jdbc_bum;
 
-import static space.jbpark.jdbc_bum.ConnectionManager.closeConnection;
-import static space.jbpark.jdbc_bum.ConnectionManager.openConnection;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-class CountriesLoader {
+class CountriesLoader extends JdbcDaoSupport {
   // @formatter:off
   private static final String LOAD_COUNTRIES_SQL = 
-      "insert into country (name, code_name) values (?, ?)";
+      "insert into country (name, code_name) values ";
   public static final String[][] COUNTRY_INIT_DATA = 
-    { { "호주", "AU" }, { "대한민국", "KR" }, { "일본", "JP" }, { "중국", "CN" }, 
-        { "미국", "US" }, { "영국", "UK" } };
+    { { "호주", "AU" }, { "대한민국", "KR" }, { "일본", "JP" },
+        { "중국", "CN" }, { "미국", "US" }, { "영국", "UK" } };
   // @formatter:on
 
   public void loadCountries() {
-    try {
-      Connection connection = openConnection();
-      for (String[] countryData : COUNTRY_INIT_DATA) {
-        PreparedStatement statement = connection
-            .prepareStatement(LOAD_COUNTRIES_SQL);
-        statement.setString(1, countryData[0]);
-        statement.setString(2, countryData[1]);
-        statement.executeUpdate();
-        statement.close();
-      }
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    } finally {
-      closeConnection();
+    for (String[] countryData : COUNTRY_INIT_DATA) {
+      String sql = LOAD_COUNTRIES_SQL + "('" + countryData[0] + "', '"
+          + countryData[1] + "');";
+      getJdbcTemplate().execute(sql);
     }
   }
 }
