c022c3b91a66d9a4eedcf48a5ed81f4a0e283f0f
osparking <jbpark03@gmail.com>
2024-02-03 13:05:37 +0900

    국가명으로 목록채취 봄 JDBC 활용 수정

--- src/main/java/space/jbpark/jdbc_bum/dao/CountryDao.java
+++ src/main/java/space/jbpark/jdbc_bum/dao/CountryDao.java
@@ -1,12 +1,10 @@
 package space.jbpark.jdbc_bum.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import space.jbpark.jdbc_bum.model.Country;
@@ -17,29 +15,14 @@ public class CountryDao extends JdbcDaoSupport {
   private static final CountryRowMapper COUNTRY_ROW_MAPPER = new CountryRowMapper();
 
   public List<Country> getCountryByName(String name) {
-    List<Country> countries = new ArrayList<>();
 
-    try {
-        Connection connection = openConnection();
-        PreparedStatement statement = connection
-                .prepareStatement(GET_COUNTRIES_BY_NAME);
-        StringBuffer searchName = new StringBuffer("%");
-        searchName.append(name);
-        searchName.append("%");
-        statement.setString(1, searchName.toString());
-        ResultSet result = statement.executeQuery();
-        while (result.next()) {
-            countries.add(new Country(result.getString("name"),
-                    result.getString("code_name")));
-        }
-        statement.close();
-    } catch (SQLException e) {
-        throw new RuntimeException(e);
-    } finally {
-        closeConnection();
-    }
-    return countries;
-}
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(
+        getDataSource());
+    SqlParameterSource source = new MapSqlParameterSource("name",
+        "%" + name + "%");
+    return namedParameterJdbcTemplate.query(GET_COUNTRIES_BY_NAME, source,
+        COUNTRY_ROW_MAPPER);
+  }
 
   public List<Country> getCountries() {
     List<Country> countries = getJdbcTemplate().query(GET_ALL_COUNTRIES,
