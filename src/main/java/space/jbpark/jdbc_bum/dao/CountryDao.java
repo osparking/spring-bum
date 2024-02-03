842f4de47662d0a44a4a248fe641851fb30ed0b5
osparking <jbpark03@gmail.com>
2024-02-03 12:38:22 +0900

    봄제공 JdbcDaoSupport 활용 Dao 클래스 수정

--- src/main/java/space/jbpark/jdbc_bum/dao/CountryDao.java
+++ src/main/java/space/jbpark/jdbc_bum/dao/CountryDao.java
@@ -1,8 +1,5 @@
 package space.jbpark.jdbc_bum.dao;
 
-import static space.jbpark.jdbc_bum.ConnectionManager.closeConnection;
-import static space.jbpark.jdbc_bum.ConnectionManager.openConnection;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,58 +7,43 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
 import space.jbpark.jdbc_bum.model.Country;
 
-public class CountryDao {
-	// @formatter:off
-	private static final String GET_ALL_COUNTRIES = "select * from country";
-	private static final String GET_COUNTRIES_BY_NAME = 
-			"select * from country where name like ?";
-	// @formatter:on
+public class CountryDao extends JdbcDaoSupport {
+  private static final String GET_ALL_COUNTRIES = "select * from country";
+  private static final String GET_COUNTRIES_BY_NAME = "select * from country where name like :name";
+  private static final CountryRowMapper COUNTRY_ROW_MAPPER = new CountryRowMapper();
 
-	public List<Country> getCountryByName(String name) {
-		List<Country> countries = new ArrayList<>();
+  public List<Country> getCountryByName(String name) {
+    List<Country> countries = new ArrayList<>();
 
-		try {
-			Connection connection = openConnection();
-			PreparedStatement statement = connection
-					.prepareStatement(GET_COUNTRIES_BY_NAME);
-			StringBuffer searchName = new StringBuffer("%");
-			searchName.append(name);
-			searchName.append("%");
-			statement.setString(1, searchName.toString());
-			ResultSet result = statement.executeQuery();
-			while (result.next()) {
-				countries.add(new Country(result.getString("name"),
-						result.getString("code_name")));
-			}
-			statement.close();
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		} finally {
-			closeConnection();
-		}
-		return countries;
-	}
+    try {
+        Connection connection = openConnection();
+        PreparedStatement statement = connection
+                .prepareStatement(GET_COUNTRIES_BY_NAME);
+        StringBuffer searchName = new StringBuffer("%");
+        searchName.append(name);
+        searchName.append("%");
+        statement.setString(1, searchName.toString());
+        ResultSet result = statement.executeQuery();
+        while (result.next()) {
+            countries.add(new Country(result.getString("name"),
+                    result.getString("code_name")));
+        }
+        statement.close();
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
+    } finally {
+        closeConnection();
+    }
+    return countries;
+}
 
-	public List<Country> getCountries() {
-		List<Country> countries = new ArrayList<>();
-
-		try {
-			Connection connection = openConnection();
-			PreparedStatement statement = connection
-					.prepareStatement(GET_ALL_COUNTRIES);
-			ResultSet result = statement.executeQuery();
-			while (result.next()) {
-				countries.add(new Country(result.getString("name"),
-						result.getString("code_name")));
-			}
-			statement.close();
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		} finally {
-			closeConnection();
-		}
-		return countries;
-	}
+  public List<Country> getCountries() {
+    List<Country> countries = getJdbcTemplate().query(GET_ALL_COUNTRIES,
+        COUNTRY_ROW_MAPPER);
+    return countries;
+  }
 }
